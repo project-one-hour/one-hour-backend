1e7362c43217fe156008c4db1a7d7dd100d11075
mongmeo-dev <mongmeo.dev@gmail.com>
2024-04-12 20:42:59 +0900

    initial commit

---
+++ src/main/java/com/project1hour/api/OneHourApiServerApplication.java
@@ -0,0 +1,13 @@
+package com.project1hour.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OneHourApiServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OneHourApiServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/project1hour/api/OneHourApiServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project1hour.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OneHourApiServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
