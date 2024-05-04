bf878ca1bcf623f846cb1c3aca77c0ad72d9bd8c
HiiWee <wpdlzhf159@gmail.com>
2024-05-04 20:04:11 +0900

    chore: rest docs gradle 세팅

--- src/main/java/com/project1hour/api/core/implement/auth/AuthReader.java
+++ src/main/java/com/project1hour/api/core/implement/auth/AuthReader.java
@@ -2,11 +2,11 @@ package com.project1hour.api.core.implement.auth;
 
 import com.project1hour.api.core.domain.auth.AuthProvider;
 import com.project1hour.api.core.domain.auth.AuthProviderRepository;
+import com.project1hour.api.core.domain.member.Member;
 import com.project1hour.api.core.implement.auth.dto.SocialInfo;
 import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
-import org.springframework.transaction.annotation.Transactional;
 
 @Component
 @RequiredArgsConstructor
@@ -14,7 +14,6 @@ public class AuthReader {
 
     private final AuthProviderRepository authProviderRepository;
 
-    @Transactional
     public Optional<Long> readExistsMemberId(final SocialInfo socialInfo) {
         Optional<AuthProvider> existsAuthProvider = authProviderRepository.findByProviderId(
                 socialInfo.getProviderId());
@@ -23,6 +22,7 @@ public class AuthReader {
             existsAuthProvider = authProviderRepository.findByEmail(socialInfo.getEmail());
         }
 
-        return existsAuthProvider.map(authProvider -> authProvider.getMember().getId());
+        return existsAuthProvider.map(AuthProvider::getMember)
+                .map(Member::getId);
     }
 }
