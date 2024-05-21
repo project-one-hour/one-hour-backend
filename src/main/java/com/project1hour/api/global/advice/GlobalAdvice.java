4053ddee37f9a24d56cb5b3b96612c131ae348bf
HiiWee <wpdlzhf159@gmail.com>
2024-05-22 03:18:40 +0900

    feat: 예상치 못한 서버 에러에 대한 핸들링 추가

--- src/main/java/com/project1hour/api/global/advice/GlobalAdvice.java
+++ src/main/java/com/project1hour/api/global/advice/GlobalAdvice.java
@@ -3,6 +3,8 @@ package com.project1hour.api.global.advice;
 
 import static com.project1hour.api.global.advice.ErrorCode.METHOD_ARGUMENT_NOT_VALID;
 
+import java.io.PrintWriter;
+import java.io.StringWriter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,7 +17,7 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class GlobalAdvice {
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<ErrorResponse> handleMethodArgumentNotValid(MethodArgumentNotValidException e) {
+    public ResponseEntity<ErrorResponse> handleMethodArgumentNotValid(final MethodArgumentNotValidException e) {
         String errorMessage = e.getFieldErrors()
                 .getFirst()
                 .getDefaultMessage();
@@ -26,23 +28,33 @@ public class GlobalAdvice {
     }
 
     @ExceptionHandler(UnauthorizedException.class)
-    public ResponseEntity<ErrorResponse> handleUnauthorized(UnauthorizedException e) {
+    public ResponseEntity<ErrorResponse> handleUnauthorized(final UnauthorizedException e) {
         log.warn("ERROR CODE {} : {}", e.getErrorCode(), e.getMessage());
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
                 .body(ErrorResponse.of(e.getMessage(), e.getErrorCode()));
     }
 
     @ExceptionHandler(BusinessException.class)
-    public ResponseEntity<ErrorResponse> handleBusiness(BusinessException e) {
+    public ResponseEntity<ErrorResponse> handleBusiness(final BusinessException e) {
         log.warn("ERROR CODE {} : {}", e.getErrorCode(), e.getMessage());
         return ResponseEntity.status(e.getStatusCode())
                 .body(ErrorResponse.of(e.getMessage(), e.getErrorCode()));
     }
 
     @ExceptionHandler(InfraStructureException.class)
-    public ResponseEntity<ErrorResponse> handleInfraStructure(InfraStructureException e) {
+    public ResponseEntity<ErrorResponse> handleInfraStructure(final InfraStructureException e) {
         log.warn("ERROR CODE {} : {}", e.getErrorCode(), e.getMessage());
         return ResponseEntity.internalServerError()
                 .body(ErrorResponse.of(e.getMessage(), e.getErrorCode()));
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> handleInternalServerError(final Exception e) {
+        StringWriter out = new StringWriter();
+        PrintWriter printWriter = new PrintWriter(out);
+        e.printStackTrace(printWriter);
+        log.error("Stack Trace : {}", out);
+        return ResponseEntity.internalServerError()
+                .body(ErrorResponse.of(e.getMessage(), ErrorCode.INTERNAL_SERVER_ERROR.getErrorCode()));
     }
 }
