3ea0db949c3b1180381f2a43ceb1d7f7e9f67e24
HiiWee <wpdlzhf159@gmail.com>
2024-05-22 02:52:36 +0900

    refactor: 예외에 대한 로깅 레벨 warn으로 설정

--- src/main/java/com/project1hour/api/global/advice/GlobalAdvice.java
+++ src/main/java/com/project1hour/api/global/advice/GlobalAdvice.java
@@ -20,21 +20,21 @@ public class GlobalAdvice {
                 .getFirst()
                 .getDefaultMessage();
 
-        log.error("ERROR CODE {} : {}", METHOD_ARGUMENT_NOT_VALID.getErrorCode(), errorMessage);
+        log.warn("ERROR CODE {} : {}", METHOD_ARGUMENT_NOT_VALID.getErrorCode(), errorMessage);
         return ResponseEntity.badRequest()
                 .body(new ErrorResponse(errorMessage, METHOD_ARGUMENT_NOT_VALID.getErrorCode()));
     }
 
     @ExceptionHandler(UnauthorizedException.class)
     public ResponseEntity<ErrorResponse> handleUnauthorized(UnauthorizedException e) {
-        log.info("ERROR CODE {} : {}", e.getErrorCode(), e.getMessage());
+        log.warn("ERROR CODE {} : {}", e.getErrorCode(), e.getMessage());
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
                 .body(ErrorResponse.of(e.getMessage(), e.getErrorCode()));
     }
 
     @ExceptionHandler(BusinessException.class)
     public ResponseEntity<ErrorResponse> handleBusiness(BusinessException e) {
-        log.info("ERROR CODE {} : {}", e.getErrorCode(), e.getMessage());
+        log.warn("ERROR CODE {} : {}", e.getErrorCode(), e.getMessage());
         return ResponseEntity.status(e.getStatusCode())
                 .body(ErrorResponse.of(e.getMessage(), e.getErrorCode()));
     }
