547524b975683a9d54dc3c666cb614124e6a63b3
HiiWee <wpdlzhf159@gmail.com>
2024-05-26 21:54:49 +0900

    refactor: jdbcTemplate batch insert를 통해 회원가입시 관심사를 한번에 등록

--- src/main/java/com/project1hour/api/core/infrastructure/member/jpa/CoreMemberInterestRepository.java
+++ src/main/java/com/project1hour/api/core/infrastructure/member/jpa/CoreMemberInterestRepository.java
@@ -2,8 +2,12 @@ package com.project1hour.api.core.infrastructure.member.jpa;
 
 import com.project1hour.api.core.domain.member.MemberInterest;
 import com.project1hour.api.core.domain.member.MemberInterestRepository;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.jdbc.core.BatchPreparedStatementSetter;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -11,9 +15,19 @@ import org.springframework.stereotype.Repository;
 public class CoreMemberInterestRepository implements MemberInterestRepository {
 
     private final JpaMemberInterestRepository jpaMemberInterestRepository;
+    private final JdbcTemplate jdbcTemplate;
 
     @Override
-    public List<MemberInterest> saveAll(final List<MemberInterest> memberInterests) {
-        return jpaMemberInterestRepository.saveAll(memberInterests);
+    public void saveAll(final List<MemberInterest> memberInterests) {
+        String sql = "INSERT INTO member_interest (member_id, interest_id) VALUES (?, ?)";
+        jdbcTemplate.batchUpdate(
+                sql,
+                memberInterests,
+                memberInterests.size(),
+                (ps, memberInterest) -> {
+                    ps.setLong(1, memberInterest.getMember().getId());
+                    ps.setLong(2, memberInterest.getInterest().getId());
+                }
+        );
     }
 }
