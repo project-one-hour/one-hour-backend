ba5089ee395f5cc6519086d3b1792372a8f54f1f
DDRRDDDD <youngmin4018@gmail.com>
2024-09-09 19:46:46 +0900

    refactor : SocialProfileId 패키지 구조 변경에 따른 구현체 import 변경

--- src/main/java/com/project1hour/api/core/infrastructure/user/client/KakaoSocialProfileId.java
+++ src/main/java/com/project1hour/api/core/infrastructure/user/client/KakaoSocialProfileId.java
@@ -1,7 +1,7 @@
 package com.project1hour.api.core.infrastructure.user.client;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.project1hour.api.core.application.user.api.SocialProfileId;
+import com.project1hour.api.core.application.user.api.data.SocialProfileId;
 
 public record KakaoSocialProfileId(
         @JsonProperty("id") String id) implements SocialProfileId {
