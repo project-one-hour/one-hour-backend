33e6d60421808ae587a8188b6f3044e9686fc73b
DDRRDDDD <youngmin4018@gmail.com>
2024-09-09 19:48:28 +0900

    refactor : SocialProfileId 패키지 구조 변경에 따른 import 수정

--- src/main/java/com/project1hour/api/core/application/user/api/OauthClient2.java
+++ src/main/java/com/project1hour/api/core/application/user/api/OauthClient2.java
@@ -1,6 +1,8 @@
 package com.project1hour.api.core.application.user.api;
 
 
+import com.project1hour.api.core.application.user.api.data.SocialProfileId;
+
 public interface OauthClient2 {
 
     boolean isSupport(String provider);
