a63376d05feb349a213d0c6a014a20ea1b49d147
DDRRDDDD <youngmin4018@gmail.com>
2024-10-05 22:08:30 +0900

    refactor : 이미지 유효성 코드 삭제로 Disabled함
    - 추후 필터 테스트에서 진행해야함

--- src/test/java/com/project1hour/api/core/application/user/ProfileImageConfigurationUseCaseTest.java
+++ src/test/java/com/project1hour/api/core/application/user/ProfileImageConfigurationUseCaseTest.java
@@ -2,7 +2,6 @@ package com.project1hour.api.core.application.user;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.mockito.BDDMockito.given;
 
 import com.project1hour.api.core.application.user.component.TestUserEventListener;
 import com.project1hour.api.global.advice.BadRequestException;
@@ -11,6 +10,7 @@ import java.io.InputStream;
 import java.util.List;
 import java.util.stream.Stream;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Test;
@@ -20,7 +20,6 @@ import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
-import org.springframework.boot.test.mock.mockito.MockBean;
 
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 @SpringBootTest(webEnvironment = WebEnvironment.NONE)
@@ -32,20 +31,20 @@ public class ProfileImageConfigurationUseCaseTest {
     @Autowired
     private TestUserEventListener testUserEventListener;
 
-    @MockBean
-    private ProfileImageClient profileImageClient;
-
     @BeforeEach
     void beforeEach() {
         testUserEventListener.reset();
     }
 
+    /**
+     * TODO : 추후 필터 테스트에서 진행해야함
+     */
     @Test
+    @Disabled
     void 유효하지_않은_프로필_이미지가_존재할_때_예외가_발생한다() {
         // given
         byte[] dummyData = "잘못된 이미지 파일 두둥".getBytes();
         List<InputStream> dummyImages = List.of(new ByteArrayInputStream(dummyData));
-        given(profileImageClient.hasInvalidImages(dummyImages)).willReturn(true);
 
         // expect
         assertThatThrownBy(() -> profileImageConfigurationUseCase.configureProfileImages(dummyImages))
@@ -56,9 +55,6 @@ public class ProfileImageConfigurationUseCaseTest {
     @ParameterizedTest
     @MethodSource("getDummyImages")
     void 프로필_이미지를_이미지_ID로_변환할_수_있다(List<InputStream> dummyImages) {
-        // given
-        given(profileImageClient.hasInvalidImages(dummyImages)).willReturn(false);
-
         //when
         List<Long> result = profileImageConfigurationUseCase.configureProfileImages(dummyImages);
 
