ef3c0104f8331a1eba432a489949946770bace12
DDRRDDDD <youngmin4018@gmail.com>
2024-10-10 01:02:56 +0900

    fix: WebConfig 비활성화

--- src/main/java/com/project1hour/api/global/config/WebConfig.java
+++ src/main/java/com/project1hour/api/global/config/WebConfig.java
@@ -4,12 +4,11 @@ import com.project1hour.api.core.presentation.auth.AuthenticationArgumentResolve
 import com.project1hour.api.core.presentation.auth.AuthenticationInterceptor;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Configuration
+//@Configuration
 @RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
 
