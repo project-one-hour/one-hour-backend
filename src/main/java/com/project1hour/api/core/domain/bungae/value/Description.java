9d8a47235ae0c5f30e141f8f9cf278f02ce05316
DDRRDDDD <youngmin4018@gmail.com>
2024-10-18 16:01:35 +0900

    feat : 컬럼 이름 (description) 추가

--- src/main/java/com/project1hour/api/core/domain/bungae/value/Description.java
+++ src/main/java/com/project1hour/api/core/domain/bungae/value/Description.java
@@ -1,10 +1,13 @@
 package com.project1hour.api.core.domain.bungae.value;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
 import jakarta.persistence.Lob;
 
+@Embeddable
 public record Description(
-        @Lob @Column
+        @Lob
+        @Column(name = "description")
         String value
 ) {
 }
