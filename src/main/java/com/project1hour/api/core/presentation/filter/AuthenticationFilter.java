d9ff4d52d64ca67669882f29a923110e7da4ee36
DDRRDDDD <youngmin4018@gmail.com>
2024-11-11 00:59:17 +0900

    delete : 사용하지 않은 로깅 어노테이션 제거

--- src/main/java/com/project1hour/api/core/presentation/filter/AuthenticationFilter.java
+++ src/main/java/com/project1hour/api/core/presentation/filter/AuthenticationFilter.java
@@ -14,10 +14,8 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
-@Slf4j
 @Component
 @RequiredArgsConstructor
 public class AuthenticationFilter extends AnnotatedUrlMappingFilter<MemberOnly> {
