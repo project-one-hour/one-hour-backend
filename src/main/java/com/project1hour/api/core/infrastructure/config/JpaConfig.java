8b9983b3b43d461f2ec6a5f68bdda46ed625827d
DDRRDDDD <youngmin4018@gmail.com>
2024-11-13 17:02:19 +0900

    docs: 인프라 레이어 config 패키지로 이동

--- src/main/java/com/project1hour/api/core/infrastructure/config/JpaConfig.java
+++ src/main/java/com/project1hour/api/core/infrastructure/config/JpaConfig.java
@@ -1,4 +1,4 @@
-package com.project1hour.api.global.config;
+package com.project1hour.api.core.infrastructure.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
