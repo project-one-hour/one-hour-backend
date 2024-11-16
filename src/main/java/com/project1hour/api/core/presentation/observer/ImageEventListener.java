ca7bf6c44caf041b9839156b1c0c51e21c9c872b
DDRRDDDD <youngmin4018@gmail.com>
2024-11-17 02:30:22 +0900

    refactor : 트랜잭션 커밋 후 해당 이벤트가 처리되도록 변경

--- src/main/java/com/project1hour/api/core/presentation/observer/ImageEventListener.java
+++ src/main/java/com/project1hour/api/core/presentation/observer/ImageEventListener.java
@@ -3,8 +3,8 @@ package com.project1hour.api.core.presentation.observer;
 import com.project1hour.api.core.application.image.exports.ImageUploadEventHandler;
 import com.project1hour.api.core.application.user.model.event.ProfileImageConfiguredEvents;
 import lombok.RequiredArgsConstructor;
-import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.event.TransactionalEventListener;
 
 @Component
 @RequiredArgsConstructor
@@ -12,7 +12,7 @@ public class ImageEventListener {
 
     private final ImageUploadEventHandler imageUploadEventHandler;
 
-    @EventListener(ProfileImageConfiguredEvents.class)
+    @TransactionalEventListener(ProfileImageConfiguredEvents.class)
     public void uploadProfileImage(final ProfileImageConfiguredEvents events) {
         events.value().stream()
                 .map(event -> new ImageUploadEventHandler.PayLoad(event.imageId(), event.imageInput()))
