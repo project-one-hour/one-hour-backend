aa39615d30dcce1a3491bfa6870f7f07fb95d9e7
DDRRDDDD <youngmin4018@gmail.com>
2024-11-29 02:26:34 +0900

    refactor : boolean primitive 타입을 Object 타입으로 변경

--- src/main/java/com/project1hour/api/core/application/user/model/event/UserUpdatedEvent.java
+++ src/main/java/com/project1hour/api/core/application/user/model/event/UserUpdatedEvent.java
@@ -4,9 +4,9 @@ import com.project1hour.api.core.domain.user.UserDomainEvent;
 import com.project1hour.api.core.domain.user.value.UserId;
 import java.time.LocalDateTime;
 
-public record UserUpdatedEvent(UserId userId, boolean isNew, LocalDateTime occurredAt) implements UserDomainEvent {
+public record UserUpdatedEvent(UserId userId, Boolean isNew, LocalDateTime occurredAt) implements UserDomainEvent {
 
-    public UserUpdatedEvent(final UserId userId, final boolean isNew) {
+    public UserUpdatedEvent(final UserId userId, final Boolean isNew) {
         this(userId, isNew, LocalDateTime.now());
     }
 
